423b8cd3a84a21ec877d1fcddf249ca180081e8b
axelg99 <58867622+axelg99@users.noreply.github.com>
2021-05-07 18:01:53 +0200

    Histoire de modifier un peu la page...

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          TP noté 07/05/2021
         </p>
         <a
           className="App-link"
